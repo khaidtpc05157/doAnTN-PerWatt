fc380486ec73759840a6d8ead2f48f98e9c3e148
toanvkpc05433 <toanvkpc05433@fpt.edu.vn>
2024-07-26 23:08:30 +0700

    newUpdate

---
+++ src/components/Order/Body/InfoBody.js
@@ -0,0 +1,209 @@
+import React from "react";
+import "./InfoBody.css";
+
+const InfoBody = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="card mt-3">
+          <div className="card-body">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">
+                    <input className="form-check-input" type="checkbox" />
+                  </th>
+                  <th scope="col">Sản phẩm</th>
+                  <th scope="col">Đơn giá</th>
+                  <th scope="col">Số lượng</th>
+                  <th scope="col">Thành tiền</th>
+                  <th scope="col">Hành động</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr id="collum">
+                  <td scope="row">
+                    <input className="form-check-input" type="checkbox" />
+                  </td>
+                  <td>
+                    <div className="d-flex">
+                      <img
+                        src="https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lhu7wmrlv2x16e"
+                        style={{ height: "100px" }}
+                        className="me-2"
+                        id="img"
+                      />
+                      <h5>Pixel 6 pro 8/128</h5>
+                    </div>
+                  </td>
+                  <td>100.000VNĐ</td>
+                  <td>10</td>
+                  <td>100.0000VNĐ</td>
+                  <td>
+                    <button className="btn btn-danger" id="delete">Xóa</button>
+                  </td>
+                </tr>
+                <tr id="collum">
+                  <td scope="row">
+                    <input className="form-check-input" type="checkbox" />
+                  </td>
+                  <td>
+                    <div className="d-flex">
+                      <img
+                        src="https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lhu7wmrlv2x16e"
+                        style={{ height: "100px" }}
+                        className="me-2"
+                        id="img"
+                      />
+                      <h5>Pixel 6 pro 8/128</h5>
+                    </div>
+                  </td>
+                  <td>100.000VNĐ</td>
+                  <td>10</td>
+                  <td>100.0000VNĐ</td>
+                  <td>
+                    <button className="btn btn-danger" id="delete">Xóa</button>
+                  </td>
+                </tr>
+                <tr id="collum">
+                  <td scope="row">
+                    <input className="form-check-input" type="checkbox" />
+                  </td>
+                  <td>
+                    <div className="d-flex">
+                      <img
+                        src="https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lhu7wmrlv2x16e"
+                        style={{ height: "100px" }}
+                        className="me-2"
+                        id="img"
+                      />
+                      <h5>Pixel 6 pro 8/128</h5>
+                    </div>
+                  </td>
+                  <td>100.000VNĐ</td>
+                  <td>10</td>
+                  <td>100.0000VNĐ</td>
+                  <td>
+                    <button className="btn btn-danger" id="delete">Xóa</button>
+                  </td>
+                </tr>
+                <tr id="collum">
+                  <td scope="row">
+                    <input className="form-check-input" type="checkbox" />
+                  </td>
+                  <td>
+                    <div className="d-flex">
+                      <img
+                        src="https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lhu7wmrlv2x16e"
+                        style={{ height: "100px" }}
+                        className="me-2"
+                        id="img"
+                      />
+                      <h5>Pixel 6 pro 8/128</h5>
+                    </div>
+                  </td>
+                  <td>100.000VNĐ</td>
+                  <td>10</td>
+                  <td>100.0000VNĐ</td>
+                  <td>
+                    <button className="btn btn-danger" id="delete">Xóa</button>
+                  </td>
+                </tr>
+                <tr id="collum">
+                  <td scope="row">
+                    <input className="form-check-input" type="checkbox" />
+                  </td>
+                  <td>
+                    <div className="d-flex">
+                      <img
+                        src="https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lhu7wmrlv2x16e"
+                        style={{ height: "100px" }}
+                        className="me-2"
+                        id="img"
+                      />
+                      <h5>Pixel 6 pro 8/128</h5>
+                    </div>
+                  </td>
+                  <td>100.000VNĐ</td>
+                  <td>10</td>
+                  <td>100.0000VNĐ</td>
+                  <td>
+                    <button className="btn btn-danger" id="delete">Xóa</button>
+                  </td>
+                </tr>
+                <tr id="collum">
+                  <td scope="row">
+                    <input className="form-check-input" type="checkbox" />
+                  </td>
+                  <td>
+                    <div className="d-flex">
+                      <img
+                        src="https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lhu7wmrlv2x16e"
+                        style={{ height: "100px" }}
+                        className="me-2"
+                        id="img"
+                      />
+                      <h5>Pixel 6 pro 8/128</h5>
+                    </div>
+                  </td>
+                  <td>100.000VNĐ</td>
+                  <td>10</td>
+                  <td>100.0000VNĐ</td>
+                  <td>
+                    <button className="btn btn-danger" id="delete">Xóa</button>
+                  </td>
+                </tr>
+                <tr id="collum">
+                  <td scope="row">
+                    <input className="form-check-input" type="checkbox" />
+                  </td>
+                  <td>
+                    <div className="d-flex">
+                      <img
+                        src="https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lhu7wmrlv2x16e"
+                        style={{ height: "100px" }}
+                        className="me-2"
+                        id="img"
+                      />
+                      <h5>Pixel 6 pro 8/128</h5>
+                    </div>
+                  </td>
+                  <td>100.000VNĐ</td>
+                  <td>10</td>
+                  <td>100.0000VNĐ</td>
+                  <td>
+                    <button className="btn btn-danger" id="delete">Xóa</button>
+                  </td>
+                </tr>
+                <tr id="collum">
+                  <td scope="row">
+                    <input className="form-check-input" type="checkbox" />
+                  </td>
+                  <td>
+                    <div className="d-flex">
+                      <img
+                        src="https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lhu7wmrlv2x16e"
+                        style={{ height: "100px" }}
+                        className="me-2"
+                        id="img"
+                      />
+                      <h5>Pixel 6 pro 8/128</h5>
+                    </div>
+                  </td>
+                  <td>100.000VNĐ</td>
+                  <td>10</td>
+                  <td>100.0000VNĐ</td>
+                  <td>
+                    <button className="btn btn-danger" id="delete">Xóa</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBody;
